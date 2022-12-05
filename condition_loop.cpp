181c7549fb63efb1296dc4976d3d981090373564
Divyaparadkar@007 <114057036+divgitp@users.noreply.github.com>
2022-12-06 00:41:36 +0530

    Add files via upload

---
+++ condition_loop.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int a ,b;
+  
+  cout<<"Enter the value of a"<<endl;
+  cin>>a;
+    cout<<"Enter the value of b "<<endl;
+    cin>>b;
+
+    if (a>b)
+    {
+    cout<<"A is greater"<<endl;
+    }
+    else
+    {
+        cout<<"b is greater"<<endl;
+    }
+     }
+
