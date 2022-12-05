31468a542ebfef63aaf64724030fe8b728f66285
Divyaparadkar@007 <114057036+divgitp@users.noreply.github.com>
2022-12-06 00:23:13 +0530

    Add files via upload

---
+++ bool_1.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a= 3;
+    int b=5;
+bool first =(a==b);
+cout<<first<<endl;
+
+bool second =(a<=b);
+cout<<second<<endl;
+
+bool third =(a>=b);
+cout<<third<<endl;
+
+bool fourth =(a<<b);
+cout<<fourth<<endl;
+
+bool sixth =(a!=b);
+cout<<sixth<<endl;
+}
