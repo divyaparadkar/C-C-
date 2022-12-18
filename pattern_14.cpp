8bb7126bfead3de90ad2cc4f3179f9bd62994f72
Divyaparadkar@007 <114057036+divyaparadkar@users.noreply.github.com>
2022-12-18 13:24:08 +0530

    Add files via upload

---
+++ pattern_14.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int i =1;
+    
+    while (i<=n)
+    {
+        int j=1;
+        int value = i;
+        while (j<=i)
+        {
+            cout<<value<<" ";
+            value = value +1;
+            j =j+1;
+        }
+        
+        cout<<endl;
+        i=i+1;
+    }
+    
+}
